a7ba3cb15994b20bf372b98f21d2629b1e0c3dac
michalkoziarski <michalkoziarski@gmail.com>
2018-02-25 20:12:09 +0100

    adjusted parameters and their default values

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 class MarineSnowCNN:
-    def __init__(self, inputs, k=3, n_3d_layers=19, n_2d_layers=1, n_filters=64, n_input_channels=3,
+    def __init__(self, inputs, k=3, n_3d_layers=20, n_2d_layers=0, n_filters=64, n_input_channels=3,
                  n_output_channels=3, use_residual_connection=True, name='MarineSnowCNN'):
         if use_residual_connection:
             assert n_input_channels == n_output_channels

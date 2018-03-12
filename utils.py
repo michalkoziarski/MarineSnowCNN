e40b24f6bd494f2a90d720a000b52f3b38a00994
michalkoziarski <michal.koziarski@gmail.com>
2018-03-12 20:43:30 +0100

    median filtering changed to peripheral 3d median filtering

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ def get_network(inputs, params):
 
 def median_filter(input, kernel_size, mask, threshold=0.5):
     assert len(input.shape) == 4
+    assert input.shape[0] >= 3
 
     output = np.empty(input.shape[1:], dtype=input.dtype)
 
@@ -22,8 +23,9 @@ def median_filter(input, kernel_size, mask, threshold=0.5):
                     x_end = i + kernel_size // 2 + 1
                     y_start = np.max((j - kernel_size // 2, 0))
                     y_end = j + kernel_size // 2 + 1
+                    temporal_range = [input.shape[0] // 2 - 1, input.shape[0] // 2 + 1]
 
-                    output[i, j, channel] = np.median(input[:, x_start:x_end, y_start:y_end, channel])
+                    output[i, j, channel] = np.median(input[temporal_range, x_start:x_end, y_start:y_end, channel])
                 else:
                     output[i, j, channel] = input[input.shape[0] // 2, i, j, channel]
 
